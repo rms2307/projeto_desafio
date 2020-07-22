29bd6a0d86de408b5a2a789127f17b86eb86ac3f
Robson Silva <rms2307@outlook.com>
2020-07-22 19:47:03 -0300

    Ajustes

--- desafio/settings.py
+++ desafio/settings.py
@@ -64,6 +64,12 @@ WSGI_APPLICATION = 'desafio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -142,7 +148,7 @@ AUTH_USER_MODEL = 'perfil.User'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(),
+    'default':  dj_database_url.config(),
 }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
@@ -153,8 +159,3 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
